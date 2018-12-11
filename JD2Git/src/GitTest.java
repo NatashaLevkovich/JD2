f2049e177fff25177895ccf823efd4cbd4f0ff4a
andrewyuraga <mr.yuraga@gmail.com>
2018-12-11 20:48:33 +0300

    update class with method print

--- JD2Git/src/GitTest.java
+++ JD2Git/src/GitTest.java
@@ -1,18 +1,5 @@
 public class GitTest {
-    public static void binarySearch(){
-        test() {
-            System.out.println("help!!!!!");
-        }
-    }
-
-    private static void help () {
-        int c = 0;
-        System.out.println(c);
-    }
     public static void main(String[] args) {
-        System.out.println("Hello");
-        GitTest gitTest = new GitTest();
-        gitTest.binarySearch();
-
+        System.out.println("Two");
     }
 }
