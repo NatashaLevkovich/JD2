0d8a8ee0f77e5474e76d27e856e067e9897ae72f
wldzam <33684076+wldzam@users.noreply.github.com>
2018-12-11 20:27:52 +0300

    my commit with hello

--- JD2Git/src/GitTest.java
+++ JD2Git/src/GitTest.java
@@ -3,7 +3,7 @@ public class GitTest {
         test() {
             System.out.println("help!!!!!");
         }
-        ////////wladtest
+        ////////wladtest 111
     }
     public static void main(String[] args) {
         System.out.println("Hello");
